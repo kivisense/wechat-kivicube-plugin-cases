68460e28f282ccb708e501755cfb0b694a9e6613
王健 <1174270493@qq.com>
2022-02-14 17:05:52 +0800

    优化代码

--- tiger-year-red-envelopes/components/back-button/back-button.js
+++ tiger-year-red-envelopes/components/back-button/back-button.js
@@ -1,15 +1,15 @@
 Component({
   data: {
-    // // 胶囊按钮高度
+    // 胶囊按钮高度
     menuButtonHeight: "32px",
-    // // 左边返回按钮距屏幕顶部的距离
+    // 左边返回按钮距屏幕顶部的距离
     capsuleTop: "48px",
   },
   lifetimes: {
     attached() {
       const { top, height } = wx.getMenuButtonBoundingClientRect();
       this.setData({
-        // 胶囊按钮高度 一般是32 如果获取不到就使用32
+        // 胶囊按钮高度
         menuButtonHeight: height + "px",
         //返回按钮的位置
         capsuleTop: top + "px",
